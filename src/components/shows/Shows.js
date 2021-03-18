a8357787e21201ed8469a287ef4260722df1d8e1
Cesar Betancourt <betancourt@David-MacPro.local>
2021-03-18 16:58:24 -0600

    Add sorting of shows

--- src/components/shows/Shows.js
+++ src/components/shows/Shows.js
@@ -6,6 +6,8 @@ import ShowCard from '../showCard/ShowCard';
 const Shows = ({ title, endpoint }) => {
   const [loading, setLoading] = useState(true);
   const [shows, setShows] = useState([]);
+  const [sortName, setSortName] = useState(false);
+  const [sortUpvotes, setSortUpvotes] = useState(false);
   const url = buildUrl(endpoint);
 
   useEffect(() => {
@@ -16,21 +18,56 @@ const Shows = ({ title, endpoint }) => {
       setLoading(false);
     };
     getShows();
-  }, [url]);
+  }, [url, sortName, sortUpvotes]);
+
+  const sortbyName = () => {
+    setSortName(!sortName);
+    if (sortUpvotes) {
+      setSortUpvotes(false);
+    }
+  };
+  const sortbyUpvotes = () => {
+    setSortUpvotes(!sortUpvotes);
+    if (sortName) {
+      setSortName(false);
+    }
+  };
 
   return (
     <div>
-      <div>
-        <h2 className="shows-title">{title}</h2>
+      <div className="show-row">
+        <div className="show-image-detail-9">
+          <h2 className="shows-title">{title}</h2>
+        </div>
+        <div className="show-image-detail-3">
+          <button
+            className={`show-label-det shows-filters ${sortName ? 'active-filter ' : ''}`}
+            onClick={sortbyName}
+          >
+            Sort by Name
+          </button>
+          <button
+            className={`show-label-det shows-filters ${sortUpvotes ? 'active-filter ' : ''}`}
+            onClick={sortbyUpvotes}
+          >
+            Sort by Calfication
+          </button>
+        </div>
       </div>
       <div className="shows-slider-complete">
         {loading ? (
           <span>loading...</span>
         ) : (
           <div className="label-with-tumbs-complete">
-            {shows.map((show) => (
-              <ShowCard key={show.id} show={show} />
-            ))}
+            {sortName
+              ? shows
+                  .sort((a, b) => a.title.localeCompare(b.title))
+                  .map((show) => <ShowCard key={show.id} show={show} />)
+              : sortUpvotes
+              ? shows
+                  .sort((a, b) => b.vote_average - a.vote_average)
+                  .map((show) => <ShowCard key={show.id} show={show} />)
+              : shows.map((show) => <ShowCard key={show.id} show={show} />)}
           </div>
         )}
       </div>
